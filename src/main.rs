0a05f097bed1adab1da020c89f1dde8b98d586b6
crazycryptom <crazycryptom@outlook.com>
2024-07-22 03:30:37 -0400

    chore: fix initial prisma

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ use std::env;
 
 fn main(){
     dotenv().ok();
-    openssl_probe::init_ssl_cert_env_vars();
     let database_url = env::var("DATABASE_URL")
         .expect("DATABASE_URL must be set");
     let _connection = Client::connect(&database_url, NoTls);
