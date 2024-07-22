1dea1a6eabc6501f1eb11891c9870c10cae58f7c
crazycryptom <crazycryptom@outlook.com>
2024-07-22 05:24:10 -0400

    fix: prisma postgresql

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
-use postgres::{Client, NoTls};
-use dotenv::dotenv;
-use std::env;
+// use postgres::{Client, NoTls};
+// use dotenv::dotenv;
+// use std::env;
 
-fn main(){
-    dotenv().ok();
-    let database_url = env::var("DATABASE_URL")
-        .expect("DATABASE_URL must be set");
-    let _connection = Client::connect(&database_url, NoTls);
-    println!("Connection to the database established!");
+#[allow(warnings, unused)]
+mod prisma;
+
+use prisma::PrismaClient;
+// use prisma_client_rust::NewClientError;
+
+#[tokio::main]
+async fn main(){
+    // dotenv().ok();
+    // let database_url = env::var("DATABASE_URL")
+    //     .expect("DATABASE_URL must be set");
+    // let _connection = Client::connect(&database_url, NoTls);
+    // println!("Connection to the database established!");
+
+    let _client = PrismaClient::_builder().build().await;
 }
